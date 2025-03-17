667f36471a50b926e9c8ed3391c4463afaf3cfc9
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-03-17 15:59:06 +0000

    Adjust product_list view by adding a decorator and returning Response

--- api/views.py
+++ api/views.py
@@ -1,12 +1,13 @@
 from django.http import JsonResponse
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
 from api.serializers import ProductSerializer
 from api.models import Product
 
 
+@api_view(['GET'])
 def product_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
-    return JsonResponse({
-        'data': serializer.data
-    })
+    return Response(serializer.data)
