b01eb13e0b355a1723d05b6f579bfc7304f4b2de
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-03-21 19:43:02 +0000

    Add a SerializerMethodField to get total price for each order

--- api/serializers.py
+++ api/serializers.py
@@ -25,12 +25,26 @@ class ProductSerializer(serializers.ModelSerializer):
 class OrderItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields = ('product', 'quantity')
+        fields = (
+            'product',
+            'quantity'
+        )
         
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField()
+
+    def get_total_price(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
 
     class Meta:
         model = Order
-        fields = ('order_id', 'created_at', 'user', 'status', 'items')
+        fields = (
+            'order_id',
+            'created_at',
+            'user', 'status',
+            'items',
+            'total_price'
+        )
