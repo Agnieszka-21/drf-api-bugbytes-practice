4c81658bd37997bc9d3742b4e0d59a87f25d903d
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-03-23 15:54:55 +0000

    Change lookup url kwarg for RetrieveAPIView

--- api/urls.py
+++ api/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('products/', views.ProductListAPIView.as_view()),
     path('products/info/', views.product_info),
     # path('products/<int:pk>/', views.product_detail),
-    path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
+    path('products/<int:product_id>/', views.ProductDetailAPIView.as_view()),
     path('orders/', views.order_list),
 ]
