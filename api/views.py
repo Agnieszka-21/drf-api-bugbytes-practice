b4dd202d787fa42ea0f418167d446e4183d35e07
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-03-27 10:50:42 +0000

    Customize the generic get_permissions method so that only admins can post data

--- api/views.py
+++ api/views.py
@@ -3,7 +3,11 @@ from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import (
+    IsAuthenticated,
+    IsAdminUser,
+    AllowAny
+)
 from rest_framework.views import APIView
 
 from api.serializers import ProductSerializer, OrderSerializer, ProductInfoSerializer
@@ -14,6 +18,12 @@ from api.models import Product, Order
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 
 # @api_view(['GET'])
