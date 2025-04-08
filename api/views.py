eade49dbdaa9830f95283d51ca7ed8457479211a
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-04-08 10:08:13 +0100

    Cache product list page with cache_page decorator

--- api/views.py
+++ api/views.py
@@ -1,5 +1,7 @@
 from django.db.models import Max
 from django.shortcuts import get_object_or_404
+from django.utils.decorators import method_decorator
+from django.views.decorators.cache import cache_page
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, viewsets
 from rest_framework.decorators import api_view, action
@@ -37,7 +39,15 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
     # pagination_class.max_page_size = 6
     pagination_class = LimitOffsetPagination
 
-
+    @method_decorator(cache_page(60 * 15, key_prefix='product_list'))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
+    
+    def get_queryset(self):
+        import time
+        time.sleep(2)
+        return super().get_queryset()
+        
     def get_permissions(self):
         self.permission_classes = [AllowAny]
         if self.request.method == 'POST':
