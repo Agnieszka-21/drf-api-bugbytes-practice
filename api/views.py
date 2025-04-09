65705ce86707a22137b9bda0b63b00f48eec19b8
Agnieszka-21 <agnieszkabialekmusic@gmail.com>
2025-04-09 09:47:05 +0100

    Apply vary_on_headers decorator to control caching behavior (orders per user)

--- api/views.py
+++ api/views.py
@@ -2,6 +2,7 @@ from django.db.models import Max
 from django.shortcuts import get_object_or_404
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
+from django.views.decorators.vary import vary_on_headers
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, generics, viewsets
 from rest_framework.decorators import api_view, action
@@ -110,6 +111,12 @@ class OrderViewSet(viewsets.ModelViewSet):
     pagination_class = None
     filterset_class = OrderFilter
     filter_backends = [DjangoFilterBackend]
+
+
+    @method_decorator(cache_page(60 * 15, key_prefix='product_list'))
+    @method_decorator(vary_on_headers('Authorization'))
+    def list(self, request, *args, **kwargs):
+        return super().list(request, *args, **kwargs)
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
